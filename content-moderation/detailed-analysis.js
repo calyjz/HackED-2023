a2818a8374cf9b9b9a959429dbac9181827c02fb
Caleb Ji <30414755+calebji123@users.noreply.github.com>
2024-01-06 23:07:37 -0700

    split small

--- content-moderation/detailed-analysis.js
+++ content-moderation/detailed-analysis.js
@@ -85,3 +85,83 @@ exports.detailedAnalysis = async function(message, field, overlap) {
         channel.send({ embeds: [embed] });
     }
 }
+
+// loops to split the message into as small chunks as possible. only returns flagged
+// chunks and returns it with first and last indices
+async function splitSmall(value, overlap) {
+    var realResponses = [];
+    var responses = [{split: value, first: 0, last: value.length, result: {flagged: false} }];
+
+    while (responses.length > 0) {
+        var response = responses.shift();
+        var content = response.split;
+        var length = content.length;
+        var offset = response.first;
+        var outs = [];
+        
+        if (length > 40) {
+            var separation = Math.floor(length / 3);
+        } else if (length > 20) {
+            var separation = Math.floor(length / 2);
+        } else {
+            realResponses.push(response);
+            continue;
+        }
+        if (separation < overlap) {
+            separation = length;
+        }
+    
+        if (length < separation) return;
+    
+        var newIndex = 0;
+        var oldIndex = 0;
+        var i;
+    
+        for (i = 0; i < Math.floor(length / separation) + (Math.floor((length % separation + Math.floor(length / separation) * overlap) / separation)); i++) {
+            if ((length - (oldIndex + separation - overlap)) < separation) {
+                newIndex = length;
+            } else {
+                newIndex = oldIndex + separation;
+            }
+            if (content[oldIndex - 2]?.match(/(\w|\d|.|,)/) && content[oldIndex - 1]?.match(/[a-zA-Z]/)) {
+                first = oldIndex - 1;
+            } else {
+                first = oldIndex;
+            }
+            if (content[newIndex + 1]?.match(/(\w|\d|.|,)/) && content[newIndex]?.match(/[a-zA-Z]/)) {
+                last = newIndex + 1;
+            } else {
+                last = newIndex;
+            }
+            outs.push({value: content.substring(first, last), first: offset + first, last: offset + last});
+            
+            oldIndex = newIndex - overlap;
+        }
+    
+        var tempResponses = [];
+        var tempRes;
+        var j;
+        for (j = 0; j < outs.length; j++) {
+            if (outs[j].value != content) {
+                try {
+                    tempRes = await openai.moderations.create({ input: outs[j].value });
+                } catch (err) {
+                    console.log(err);
+                    return { error: true };
+                }
+                
+                if (tempRes?.results?.length > 0 && tempRes.results[0].flagged) {
+                    tempResponses.push({ split: outs[j].value, first: outs[j].first, last: outs[j].last, result: tempRes.results[0] });
+                }
+            }
+        }
+        if (tempResponses.length > 0) {
+            responses.push(...tempResponses);
+        } else {
+            if (response.split != value) {realResponses.push(response);}
+        }
+    }
+
+    console.log(realResponses)
+    return realResponses;
+}
